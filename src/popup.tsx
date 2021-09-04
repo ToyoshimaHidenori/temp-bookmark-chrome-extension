1faac7a7ed8d5b0411408ecae609d58c5b477d28
hidenori <h193345@edu.tut.ac.jp>
2021-09-05 02:24:41 +0900

    add: url list

--- src/popup.tsx
+++ src/popup.tsx
@@ -4,16 +4,39 @@ import ReactDOM from "react-dom";
 const Popup = () => {
   const [count, setCount] = useState(0);
   const [currentURL, setCurrentURL] = useState<string>();
+  const [bookmarks, setBookmarks] = useState<Array<string>>();
 
   useEffect(() => {
-    chrome.browserAction.setBadgeText({ text: count.toString() });
-  }, [count]);
+    chrome.browserAction.setBadgeText({ text: bookmarks?.length.toString() });
+  }, [bookmarks]);
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       setCurrentURL(tabs[0].url);
     });
+    chrome.storage.sync.get(['bookmarks'], function(result) {
+        setBookmarks(result.bookmarks)
+        console.log('Value currently is ' + result.bookmarks);
+    });
   }, []);
+
+  const addBookmark = () => {
+      if(currentURL===undefined){
+          console.error("Current url is not provided");
+          return;
+      }
+
+      let tmp_bookmarks:Array<string> = [];
+      if(bookmarks){
+          tmp_bookmarks = bookmarks.slice();
+      }
+      tmp_bookmarks.push(currentURL);
+      setBookmarks(tmp_bookmarks);
+      chrome.storage.sync.set({"bookmarks": tmp_bookmarks}, () =>{
+          console.log('added bookmark' + currentURL);
+      })
+
+  }
 
   const changeBackground = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -22,7 +45,7 @@ const Popup = () => {
         chrome.tabs.sendMessage(
           tab.id,
           {
-            color: "#555555",
+            color: "#ff5599",
           },
           (msg) => {
             console.log("result message:", msg);
@@ -38,6 +61,12 @@ const Popup = () => {
         <li>Current URL: {currentURL}</li>
         <li>Current Time: {new Date().toLocaleTimeString()}</li>
       </ul>
+        {bookmarks &&
+            bookmarks.map((bookmark, index) => (
+                <button key={index}>{bookmark}</button>
+            ))
+        }
+        <button onClick={addBookmark}>add bookmark</button>
       <button
         onClick={() => setCount(count + 1)}
         style={{ marginRight: "5px" }}
